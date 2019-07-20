982c2c84dc2ca27adceefb72b23ace794fbd2777
test0071030 <e14051033@gs.ncku.edu.tw>
2019-07-21 03:33:45 +0800

    happy

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
 var bang=1;
+var fee = [1.63, 2.1, 2.89, 3.79, 4.42, 4.83];
+var index_number = [0,240,660,1000,1400,2000];
 
 $(document).ready(function(){
   
   $("#season_not_summer").click(function() {
-    var fee = [1.63, 2.1, 2.89, 3.79, 4.42, 4.83];
-    var bang=1;
+    fee = [1.63, 2.1, 2.89, 3.79, 4.42, 4.83];
+    bang=1;
   })
   
   $("#season_summer").click(function() {
-    var fee = [1.63, 2.38, 3.52, 4.61, 5.42, 6.13];
-    var bang=1;
+    fee = [1.63, 2.38, 3.52, 4.61, 5.42, 6.13];
+    bang=2;
   })
   
   $("#season_mix").click(function() {
-    var bang=2;
+    bang=3;
   })
 
   $("#start_calculate").click(function() {
-    var milk_previous=$('#milk_previous').val();
-    var black_previous=$('#black_previous').val();
-    var pinch_previous=$('#pinch_previous').val();
-    var king_previous=$('#king_previous').val();
-
-    var milk_this=$('#milk_this').val();
-    var black_this=$('#black_this').val();
-    var pinch_this=$('#pinch_this').val();
-    var king_this=$('#king_this').val();
+    var number=[0,0,0,0,0,0];
+    var four_number=[0,0,0,0];
+    var four_sum=0;
+    var four_percent=[0,0,0,0];
+    var all_number=0;
+    var four_use=[0,0,0,0,0,0];
+    var banana=0;
+    var four_airsum=0;
+    var four_air=[0,0,0,0];
+    var four_othersum=0;
+    var four_final=[0,0,0,0];
     
+    var milk_previous=parseFloat($('#milk_previous').val());
+    var black_previous=parseFloat($('#black_previous').val());
+    var pinch_previous=parseFloat($('#pinch_previous').val());
+    var king_previous=parseFloat($('#king_previous').val());
+    var previous=[milk_previous,black_previous,pinch_previous,king_previous];
+
+    var milk_now=parseFloat($('#milk_this').val());
+    var black_now=parseFloat($('#black_this').val());
+    var pinch_now=parseFloat($('#pinch_this').val());
+    var king_now=parseFloat($('#king_this').val());
+    var now=[milk_now,black_now,pinch_now,king_now];
+
+    var sum_fee=parseFloat($('#sum_fee').val());
+
+    for (i=0;i<=3;i++){
+      four_number[i]=now[i]-previous[i];
+      four_sum+=four_number[i];
+    }
+
+    for (i=0;i<=3;i++){
+      four_percent[i]=four_number[i]/four_sum;
+    }
+    
+    if (bang==1){
+      all_number=parseInt($("#not_summer_number_all").val());
+    }
+
     if (bang==2){
-      var parta=$('#not_summer_part').val();
-      var partb=$('#summer_part').val();
+      all_number=parseInt($("#summer_number_all").val());
+    }
+
+
+    if (bang==3){
+      var parta=parseInt($('#not_summer_part').val());
+      var partb=parseInt($('#summer_part').val());
       var partall= parta+partb;
       var fee1 = 1.63;
       var fee2 = (parta*2.1+partb*2.38)/partall;
@@ -37,12 +73,71 @@ $(document).ready(function(){
       var fee4 = (parta*3.79+partb*4.61)/partall;
       var fee5 = (parta*4.42+partb*5.42)/partall;
       var fee6 = (parta*4.83+partb*6.13)/partall;
-      var fee = [fee1, fee2, fee3, fee4, fee5, fee6];
+      fee = [fee1, fee2, fee3, fee4, fee5, fee6];
+
+      all_number=parseInt($("#mix_all").val());
+    }
+
+    for (i=0;i<=5;i++){
+      
+      if (all_number<=index_number[i+1]){
+        number[i]=all_number-index_number[i];
+        break; 
+      }
+
+      else{
+       if (i==5){
+          number[5]=all_number-index_number[5];
+          break;
+        }
+
+        else{
+          number[i]=index_number[i+1]-index_number[i];
+        }
+
+      }
+    }
+
+    for (i=5;i>=0;i--){
+      if (four_sum>number[i]){
+        four_use[i]=number[i];
+        four_sum-=number[i];
+      }
+
+      else{
+        four_use[i]=four_sum;
+        break;
+      }
+    }
+
+
+
+
+    for (i=5;i>=0;i--){
+      four_airsum+=four_use[i]*fee[i];
+    }
+
+    four_othersum=sum_fee-four_airsum;
+
+    for (i=0;i<=3;i++){
+      four_air[i]=four_airsum*four_percent[i];
     }
     
+    for (i=0;i<=3;i++){
+      four_final[i]=Math.round(four_air[i]+four_othersum/4);
+    }
+
+    console.log (four_number);
+    console.log (four_sum);
+    console.log (four_percent);
+    console.log (four_use)
+    console.log (number);
     console.log (fee);
-    
-  })
+    console.log (four_airsum);
+    console.log (four_air);
+    console.log (four_othersum);
+    console.log (four_final); 
+    })
   
   
 })
